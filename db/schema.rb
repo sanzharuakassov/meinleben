6de75386d26e22ba3788b7b6e08f0b25564b8960
User <User>
2017-03-05 13:26:12 +0600

    dadada

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170226052006) do
+ActiveRecord::Schema.define(version: 20170305072111) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "password"
     t.boolean  "ssex"
     t.date     "bday"
   end
